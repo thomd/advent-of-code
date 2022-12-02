bb5f1a51800d0e55156c344883d212f70d6f30c8
Thomas Dürr <thomduerr@gmail.com>
2022-12-02 12:44:50 +0100

    day1

--- day1.sh
+++ day1.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# part 1
 awk '/[0-9]+/{c += $1} /^$/{print c; c = 0}' < input/day1.txt | sort -nr | head -n 1
+
+# part 2
+awk '/[0-9]+/{c += $1} /^$/{print c; c = 0}' < input/day1.txt | sort -nr | head -n 3 | awk '{c += $1} END {print c}'
